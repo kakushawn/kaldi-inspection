881fdcde38c86163718fb5302a1c7b6ec31d8d4a
shihchankuo <kakushawn@gmail.com>
2019-09-01 17:39:15 +0800

    Integrate confirm

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,9 +1,10 @@
-var blob, gumStream, au_show_flag = false;
+var blob, blob_confirm, gumStream, au_show_flag = false;
 var record_count = 0;
-var canvas, recorder, recorderGUI, audioContext;
+var canvas, recorder, recorder_confirm, recorderGUI, audioContext;
 var stop_rec_timer, draw_timer;
 var drawer = null;
 var command_type = "";
+var decoded_command;
 
 function drwaing() {
   recorder.getBuffer(function (buffer) {
@@ -37,7 +38,7 @@ function startRecording() {
     /* use the stream */
     input = audioContext.createMediaStreamSource(stream);
 
-    /* 
+    /*
       Create the Recorder object and configure to record mono sound (1 channel)
       Recording 2 channels  will double the file size
     */
@@ -60,6 +61,58 @@ function startRecording() {
   });
 }
 
+function startRecordingConfirm() {
+  console.log("Recording confirm command");
+  /*
+    Simple constraints object, for more advanced audio features see
+    https://addpipe.com/blog/audio-constraints-getusermedia/
+  */
+
+  var constraints = { audio: true, video: false }
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+    /*
+      create an audio context after getUserMedia is called
+      sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+      the sampleRate defaults to the one set in your OS for your playback device
+    */
+    audioContext = new AudioContext();
+
+    /*  assign to gumStream for later use  */
+    gumStream = stream;
+
+    /* use the stream */
+    input = audioContext.createMediaStreamSource(stream);
+
+    /*
+      Create the Recorder object and configure to record mono sound (1 channel)
+      Recording 2 channels  will double the file size
+    */
+    au_show_flag = false;
+    recorder_confirm = new Recorder(input, { numChannels: 1 });
+
+    //start the recording process
+    recorder_confirm.clear();
+    // canvas.style.display = 'none';
+    // $('#audio_show').empty();
+    // recorderGUI.reset();
+    // drwaing();
+    recorder_confirm.record();
+    console.log("Recording confirm started");
+    // record_count = 1;
+    stop_rec_timer = setTimeout(stopRecordingConfirm, 1000 * 3);
+
+
+
+    console.log("Finish recording confirm.");
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+
+}
+
 
 Number.prototype.zeropad = function(size) {
   var s = String(this);
@@ -68,10 +121,11 @@ Number.prototype.zeropad = function(size) {
 }
 
 function stopRecording() {
-	
+
   $('#recordButton').text('開始錄音')
   if (recorder) {
     console.log("stopRecording");
+    console.log('au_show_flag:' + au_show_flag);
 
     clearTimeout(stop_rec_timer);
     clearTimeout(draw_timer);
@@ -83,6 +137,7 @@ function stopRecording() {
     gumStream.getAudioTracks()[0].stop();
 
     recorder.exportWAV(function (blob_) {
+        console.log(blob_);
       if (au_show_flag === false) {
         au_show_flag = true;
         var url = window.URL.createObjectURL(blob_);
@@ -93,11 +148,11 @@ function stopRecording() {
         blob = blob_;
         // blob.append(blob_);
         const date_time = new Date;
-        const timestamp = date_time.getFullYear().toString() + 
-                          (date_time.getMonth() + 1).zeropad(2) + 
-                          date_time.getDate().zeropad(2) +  
-                          date_time.getHours().zeropad(2) + 
-                          date_time.getMinutes().zeropad(2) + 
+        const timestamp = date_time.getFullYear().toString() +
+                          (date_time.getMonth() + 1).zeropad(2) +
+                          date_time.getDate().zeropad(2) +
+                          date_time.getHours().zeropad(2) +
+                          date_time.getMinutes().zeropad(2) +
                           date_time.getSeconds().zeropad(2);
         $('#audio_show').append(`<br/><a href="${url}" download="demo_${timestamp}" > download WAV </a><br/>`);
       }
@@ -111,6 +166,60 @@ function stopRecording() {
   }
 }
 
+function stopRecordingConfirm() {
+  if (recorder_confirm) {
+    console.log("Stop recording confirm");
+    // console.log('au_show_flag:' + au_show_flag);
+
+    // clearTimeout(stop_rec_timer);
+    // clearTimeout(draw_timer);
+
+    //tell the recorder to stop the recording
+    recorder_confirm.stop();
+
+    //stop microphone access
+    gumStream.getAudioTracks()[0].stop();
+
+    recorder_confirm.exportWAV(function (blob_) {
+      if (au_show_flag === false) {
+        au_show_flag = true;
+        var url = window.URL.createObjectURL(blob_);
+        var au = document.createElement('audio');
+        au.controls = true;
+        au.src = url;
+        blob_confirm = blob_;
+        const fd_confirm = new FormData();
+        fd_confirm.append('wav_data', blob_confirm);
+        fd_confirm.append('command_type', 'yn_en');
+        $.ajax({
+          type: 'POST',
+          url: 'https://speechscoring.mirlab.org/ajax_record',
+          data: fd_confirm,
+          processData: false,
+          contentType: false
+        }).done(function (result_confirm) {
+            yn = result_confirm.data.message;
+            if (yn === 'yes') {
+                $('#result').text(decoded_command);
+            } else {
+                $('#result').text('請重新辨識');
+            }
+            var modal = document.getElementById("confirmModal");
+            modal.style.display = "none";
+        });
+      }
+    });
+
+
+
+    // if (record_count === 1) {
+    //   $('#recordButton').prop('disabled', false);
+    //   $('#uploadButton').prop('disabled', false);
+    // }
+
+  }
+}
+
 const wav_file_uploader = document.querySelector('#choose_wav_file');
 var choose_wav_file_ready = false;
 wav_file_uploader.addEventListener('change', (e) => {
@@ -120,7 +229,7 @@ wav_file_uploader.addEventListener('change', (e) => {
   console.log(e.target.files); // get file object
   console.log(choose_wav_file);
   $('#uploadButton').prop('disabled', false);
-  
+
   if (au_show_flag === false) {
     au_show_flag = true;
     var url = window.URL.createObjectURL(choose_wav_file);
@@ -142,7 +251,7 @@ function uploadClick() {
   if (recorder || choose_wav_file_ready) {
     $('#result').show();
     $('#result').text('處理中，請稍後...');
-    console.log(blob);
+    // console.log(blob);
     const fd = new FormData();
 
     fd.append('wav_data', blob);
@@ -151,19 +260,60 @@ function uploadClick() {
 	$('#resultDiv').hide();
     $.ajax({
       type: 'POST',
-      url: '/ajax_record',
+      url: 'https://speechscoring.mirlab.org/ajax_record',
       data: fd,
       processData: false,
       contentType: false
     }).done(function (result) {
       $('#downloadButton').prop('disabled', false);
-      $('#result').text(result.data.message);
+      decoded_command = result.data.message;
+      if (result.data.trustworthy) {
+          $('#result').text(decoded_command);
+      } else {
+          // $('#result').text(result.data.message + " (need confirm) ");
+
+          var modal = document.getElementById("confirmModal");
+
+          // Get the button that opens the modal
+          // var btn = document.getElementById("myBtn");
+
+          // Get the <span> element that closes the modal
+          var span = document.getElementsByClassName("close")[0];
+
+          // When the user clicks the button, open the modal
+          // btn.onclick = function() {
+          modal.style.display = "block";
+          // }
+
+          $('#confirm_question').text("確定是「"+result.data.message+"」嗎？ (yes/no)");
+
+          var timeleft = 3;
+          var downloadTimer = setInterval(function(){
+            timeleft--;
+            document.getElementById("confirm_timer").textContent = timeleft;
+            if(timeleft <= 0)
+              clearInterval(downloadTimer);
+          },1000);
+
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function() {
+            modal.style.display = "none";
+          }
+
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          }
+
+          startRecordingConfirm();
+      }
       if (recorder){
         recorder.clear();
       }
 
 	  $('#resultDiv').show();
-    $('#uploadButton').prop('disabled', false);
+      $('#uploadButton').prop('disabled', false);
     });
   }
 }
@@ -176,6 +326,34 @@ async function stopAndUpload() {
   stopRecording();
   await sleep(500);
   uploadClick();
+}
+
+function startTimer(duration_sec, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since
+        // startTimer() was called
+        diff = duration_sec - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        seconds = diff | 0;
+
+        // seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+
+        // if (diff <= 0) {
+        //     // add one second so that the count down starts at the full duration
+        //     // example 05:00 not 04:59
+        //     start = Date.now() + 1000;
+        // }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
 }
 
 window.onload = function init() {
