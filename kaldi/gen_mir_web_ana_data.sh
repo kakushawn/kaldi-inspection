757f723eba7a64259f5cd305b7430efe321c4f3e
shihchankuo <kakushawn@gmail.com>
2020-03-12 15:07:56 +0800

    add kaldi result maker

---
+++ kaldi/gen_mir_web_ana_data.sh
@@ -0,0 +1,28 @@
+set -e
+
+if [ "$#" -ne 4 ]; then
+    echo "usage: $0 corpus data_of_decode graph/lang decode_dir" && exit
+fi
+
+corpus=$1
+data=$2
+graph=$3
+decode_dir=$4
+
+# echo "$0 $1 $2 $3"
+if [ -f $decode_dir/../frame_subsampling_factor ]; then
+    n=`cat $decode_dir/../frame_subsampling_factor`
+    frame_shift="--frame-shift 0.0$n"
+fi
+
+steps/get_ctm_fast.sh $frame_shift --print-silence true $data $graph $decode_dir $decode_dir/ctm
+
+python local/word_ctm_to_mir_schema.py $data $decode_dir/ctm/ctm $decode_dir/mir
+
+rm -rf $decode_dir/data
+mkdir $decode_dir/data
+for f in wav.scp text utt2spk; do
+    cp $data/$f $decode_dir/data
+done
+
+echo $corpus > $decode_dir/corpus
