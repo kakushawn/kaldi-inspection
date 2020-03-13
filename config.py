1f86a4c9a3016628dddcb6838a3a4b890e6c17d3
shihchankuo <kakushawn@gmail.com>
2020-03-13 22:37:01 +0800

    remove static/uploads from config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 ROOT_FOLDER = '/Users/shawnkaku/repositories/mirlab/foxconn/demo-web/'
 STATIC_FOLDER = ROOT_FOLDER + '/static/'
-UPLOAD_FOLDER = ROOT_FOLDER + '/static/uploads'
 
 # kaldi
 DECODES_FOLDER = ROOT_FOLDER + '/kaldi/decodes/'
